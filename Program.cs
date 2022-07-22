f21038ebdac882e743d0e9f36e087c2a07feb607
Hernan Ferreyra <hernan.ferreyra@ppst.com>
2022-07-22 16:52:53 -0300

    request ftp dir list

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net.Cache;
+using System;
 using System.Net;
-
+using System.IO;
 
 
 
@@ -10,18 +14,67 @@ class Program
 {
 static void Main(string[] args)
 {
-	Console.WriteLine("Hello World!");
+	Console.WriteLine("Dependency downloader");
 	string remoteUri = "ftp://firmware.ppstinc.net%257Cfirmware.admin@firmware.ppstinc.net/SW/Installers/dependencies/";
-	string fileName = "PPS-Logo.ico", myStringWebResource = null;
-	myStringWebResource = remoteUri + fileName;
-	Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
-    Uri target = new Uri (myStringWebResource);
-	DisplayFileFromServer(target);
-	//Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+	string fileName = "PPS-Logo.ico";
+
+	SearchFTPfiles(remoteUri, fileName);
+	DisplayFileFromServer(remoteUri, fileName);
+
+
 }
 
-public static bool DisplayFileFromServer(Uri serverUri)
+public static bool SearchFTPfiles(String Addres, String Files)
 {
+	FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Addres);
+	request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+	request.Credentials = new NetworkCredential ("firmware.ppstinc.net|firmware.admin","Kydb4yvhrYQnsK8");
+
+	FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+
+	Stream responseStream = response.GetResponseStream();
+	StreamReader reader = new StreamReader(responseStream);
+	//Console.WriteLine(reader.ReadToEnd());
+	String a = reader.ReadToEnd();
+	Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
+	Console.WriteLine("the dirs: {0}",a.IndexOf("<DIR>"));
+
+	string[] words = a.Split('\n');
+
+	foreach (var word in words)
+	{
+		System.Console.WriteLine($"<{word}>");
+	}
+
+
+	string[] be = words[0].Split(' ',System.StringSplitOptions.RemoveEmptyEntries);
+	Console.WriteLine($"Dir {0}",1);
+	foreach (var i in be)
+	{
+		System.Console.WriteLine(i);
+
+	}
+
+	System.Console.WriteLine(be[be.Length -1]);
+
+
+
+	reader.Close();
+	response.Close();
+	return true;
+}
+
+public static bool DisplayFileFromServer(String Addres, String Files)
+{
+	string myStringWebResource = Addres + Files;
+	if (!Directory.Exists("dependencies"))
+	{
+		Directory.CreateDirectory("dependencies");
+	}
+	Files="dependencies/"+Files;
+	Uri serverUri = new Uri (myStringWebResource);
 	// The serverUri parameter should start with the ftp:// scheme.
 	if (serverUri.Scheme != Uri.UriSchemeFtp)
 	{
@@ -34,9 +87,13 @@ public static bool DisplayFileFromServer(Uri serverUri)
 	request.Credentials = new NetworkCredential ("firmware.ppstinc.net|firmware.admin","Kydb4yvhrYQnsK8");
 	try
 	{
-		byte [] newFileData = request.DownloadData (serverUri.ToString());
-		string fileString = System.Text.Encoding.UTF8.GetString(newFileData);
-		Console.WriteLine(fileString);
+		//byte [] newFileData = request.DownloadData (serverUri.ToString());
+		Console.WriteLine("Downloading {0}", myStringWebResource);
+		request.DownloadFile(myStringWebResource,Files);
+
+
+		//string fileString = System.Text.Encoding.UTF8.GetString(newFileData);
+		//Console.WriteLine(fileString);
 	}
 	catch (WebException e)
 	{
@@ -45,12 +102,13 @@ public static bool DisplayFileFromServer(Uri serverUri)
 	return true;
 }
 
+
+
+
+
+
+
 }
-
-
-
-
-
 }
 
 
