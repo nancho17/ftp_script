12072812971296fd02042b0e4b91f88f032cb33a
Hernan Ferreyra <hernan.ferreyra@ppst.com>
2022-07-25 16:53:03 -0300

    ftp download implemented

--- Program.cs
+++ Program.cs
@@ -1,32 +1,38 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Net.Cache;
 using System;
 using System.Net;
 using System.IO;
-
+using System.Collections.Generic;
 
 
 namespace scripts
 {
 class Program
 {
+
+static string remoteUri = "ftp://firmware.ppstinc.net%257Cfirmware.admin@firmware.ppstinc.net/SW/Installers/dependencies/Installers/";
+
 static void Main(string[] args)
 {
 	Console.WriteLine("Dependency downloader");
-	string remoteUri = "ftp://firmware.ppstinc.net%257Cfirmware.admin@firmware.ppstinc.net/SW/Installers/dependencies/";
-	string fileName = "PPS-Logo.ico";
+	//string fileName = "PPS-Logo.ico";
 
-	SearchFTPfiles(remoteUri, fileName);
-	DisplayFileFromServer(remoteUri, fileName);
+	SearchFTPfiles("");
+	//DisplayFileFromServer(remoteUri, fileName);
 
 
 }
 
-public static bool SearchFTPfiles(String Addres, String Files)
+public static bool SearchFTPfiles(String Addres)
 {
-	FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Addres);
+	System.Console.Write("Searching: ");
+	System.Console.WriteLine(remoteUri+Addres);
+
+	FtpWebRequest request = (FtpWebRequest)WebRequest.Create(remoteUri+Addres);
 	request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
 
 	request.Credentials = new NetworkCredential ("firmware.ppstinc.net|firmware.admin","Kydb4yvhrYQnsK8");
@@ -39,41 +45,64 @@ public static bool SearchFTPfiles(String Addres, String Files)
 	//Console.WriteLine(reader.ReadToEnd());
 	String a = reader.ReadToEnd();
 	Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
-	Console.WriteLine("the dirs: {0}",a.IndexOf("<DIR>"));
 
-	string[] words = a.Split('\n');
+	List<string> dirList = new List<string>();
+
+	string[] words = a.Split("\r\n",System.StringSplitOptions.RemoveEmptyEntries);
 
 	foreach (var word in words)
 	{
-		System.Console.WriteLine($"<{word}>");
+
+		string[] be = word.Split(' ',System.StringSplitOptions.RemoveEmptyEntries);
+		//System.Console.WriteLine( be[be.Length -1]);
+		if (be.Length> 4) {
+			for (int i = 4; i < be.Length; i++)
+			{
+				be[3] +=" "+ be[i];
+			}
+			System.Console.WriteLine("is {0}", be[3]);
+		}
+		if(word.Contains("<DIR>"))
+		{
+
+			dirList.Add(be[3]);
+		}
+		else
+		{
+			//System.Console.WriteLine("{0}", be[3]);
+			DisplayFileFromServer(Addres,be[3]);
+
+		};
+
 	}
-
-
-	string[] be = words[0].Split(' ',System.StringSplitOptions.RemoveEmptyEntries);
-	Console.WriteLine($"Dir {0}",1);
-	foreach (var i in be)
-	{
-		System.Console.WriteLine(i);
-
-	}
-
-	System.Console.WriteLine(be[be.Length -1]);
-
-
 
 	reader.Close();
 	response.Close();
+
+	foreach (String i in dirList)
+	{
+		//System.Console.WriteLine("{0}", i);
+		SearchFTPfiles(Addres+i+"/");
+	}
+
 	return true;
 }
 
 public static bool DisplayFileFromServer(String Addres, String Files)
 {
-	string myStringWebResource = Addres + Files;
-	if (!Directory.Exists("dependencies"))
+	string myStringWebResource = remoteUri+ Addres + Files;
+	System.Console.WriteLine("{0}", myStringWebResource );
+
+	Addres="dependencies/"+Addres;
+	if (!Directory.Exists(Addres))
 	{
-		Directory.CreateDirectory("dependencies");
+		Directory.CreateDirectory(Addres);
 	}
-	Files="dependencies/"+Files;
+	if (Files==" ")
+	{
+		return false;
+	}
+	Files=Addres+Files;
 	Uri serverUri = new Uri (myStringWebResource);
 	// The serverUri parameter should start with the ftp:// scheme.
 	if (serverUri.Scheme != Uri.UriSchemeFtp)
@@ -90,8 +119,6 @@ public static bool DisplayFileFromServer(String Addres, String Files)
 		//byte [] newFileData = request.DownloadData (serverUri.ToString());
 		Console.WriteLine("Downloading {0}", myStringWebResource);
 		request.DownloadFile(myStringWebResource,Files);
-
-
 		//string fileString = System.Text.Encoding.UTF8.GetString(newFileData);
 		//Console.WriteLine(fileString);
 	}
